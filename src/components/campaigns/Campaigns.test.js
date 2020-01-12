34a408350a39a5c78134a80e7ade74773c4109f4
Colton <colton@Coltons-MacBook-Pro.local>
2020-01-11 20:55:26 -0800

    changed package config for jest

--- src/components/campaigns/Campaigns.test.js
+++ src/components/campaigns/Campaigns.test.js
@@ -4,10 +4,10 @@ import { shallow } from "enzyme";
 import Campaigns from "./Campaigns";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+// import Enzyme from "enzyme";
+// import Adapter from "enzyme-adapter-react-16";
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
 const mockStore = configureStore();
 
